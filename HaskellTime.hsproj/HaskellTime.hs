0cc8682ee58af73ebd1ff2b71125cadfc877ca4f
Nikita Ermolenko <gnod94@gmail.com>
2017-04-01 20:05:02 +0600

    Check tuples and binders

--- HaskellTime.hsproj/HaskellTime.hs
+++ HaskellTime.hsproj/HaskellTime.hs
@@ -9,3 +9,25 @@ showResult x = "The result is " ++ (show x)
 
 showAreaOfCircle :: (Floating a, Show a) => a -> String
 showAreaOfCircle r = "The area of a circle with radius " ++ show r ++ " is about " ++ (show $ pi * (r*r)) ++ "cm^2"
+
+
+
+-- Binders
+
+pi' :: Floating a => a
+pi' = 3.1415926
+
+circleArea :: Floating a => a -> a
+circleArea diameter  = pi' * radius * radius
+  where
+    radius = diameter / 2.0       -- local binding
+    
+-- Tuples
+
+type Point = (Int, Int)
+
+origin :: Point
+origin = (0, 0)
+
+moveUp :: Point -> Int -> Point
+moveUp (x, y) distance = (x, y-distance)
--- HaskellTime.hsproj/Simple.hs
+++ HaskellTime.hsproj/Simple.hs
@@ -6,6 +6,6 @@ max x y = if x > y then x else y
 
 signum :: (Ord a, Num a) => a -> Int
 signum x 
-  | x < 0  = -1
-  | x == 0 = 0
-  | x > 0  = 1
+  | x < 0      = -1
+  | x == 0     = 0
+  | otherwise  = 1
