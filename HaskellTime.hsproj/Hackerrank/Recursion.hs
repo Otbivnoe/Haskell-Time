c96748e3669de3b1a48b9325e35fbc080563a9e2
Nikita Ermolenko <gnod94@gmail.com>
2017-04-09 15:32:45 +0600

    Complete the GCD challange

--- HaskellTime.hsproj/Hackerrank/Recursion.hs
+++ HaskellTime.hsproj/Hackerrank/Recursion.hs
@@ -13,3 +13,11 @@ fib :: Int -> Int
 fib 1 = 0
 fib 2 = 1
 fib n = fib (n-1) + fib (n-2) 
+
+-- https://www.hackerrank.com/challenges/functional-programming-warmups-in-recursion---gcd
+
+gcd' :: Integral a => a -> a -> a
+gcd' n m
+  | n == m    = n
+  | n > m     = gcd' (n-m) m
+  | n < m     = gcd' m n
