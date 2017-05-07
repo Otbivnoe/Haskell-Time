991454c601b775e8cefc92a955dcb2a95ad5892e
Nikita Ermolenko <gnod94@gmail.com>
2017-05-07 22:16:19 +0600

    Add TimeMachine model

--- HaskellTime.hsproj/Show/Models.hs
+++ HaskellTime.hsproj/Show/Models.hs
@@ -3,9 +3,9 @@
 module Show.Models
 where 
   
-data Client = GovOrg String
- | Company    String Integer Person
- | Individual Person Bool
+data Client = GovOrg     String
+            | Company    String Integer Person
+            | Individual Person Bool
  deriving Show
  
 
@@ -13,4 +13,11 @@ data Gender = Male | Female | Unknow
   deriving Show
 
 data Person = Person String String Gender
+  deriving Show
+ 
+
+data TimeMachine = TimeMachine Manufacturer Integer Bool Double
+  deriving Show
+  
+data Manufacturer = Manufacturer String
   deriving Show
